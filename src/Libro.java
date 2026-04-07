11aa1a1de34d304de046029864dd69bd2881e75a
Esteban Cisneros <estebancis02@Estebans-MacBook-Pro.local>
2026-04-07 00:10:44 -0500

    Prueba en Mac

---
+++ src/Libro.java
@@ -0,0 +1,35 @@
+public class Libro {
+    private String titulo;
+    private String autor;
+    private String categoria;
+    private int anio;
+    private int cantidadEjemplares;
+
+
+    public Libro(){
+        titulo="Java para ingenieros";
+        autor="Paulo Guerra";
+        categoria="Programación";
+        anio=2014;
+        cantidadEjemplares=5;
+    }
+
+    public String mostrarInformacion(){
+        String aux="";
+        aux="Titulo: "+titulo+"\n";
+        aux+="Autor: "+autor+"\n";
+        aux+="Categorio: "+categoria+"\n";
+        aux+="Año: "+anio+"\n";
+        aux+="Cantidad: "+cantidadEjemplares+"ejemplare(es) \n";
+        return aux;
+    }
+
+    public int prestar(){
+        if (cantidadEjemplares>0){
+            cantidadEjemplares--;
+            return cantidadEjemplares;
+        }
+        return -1; //signiica que no hay más ejemplares
+    }
+
+}
